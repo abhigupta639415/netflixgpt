9c240d0c718e81935b629070b1a6b3c54b66de45
Abhishek Gupta <abhigupta639415@gmail.com>
2026-04-20 17:09:05 +0530

    adding .env in gitignore

--- src/components/MoviesCard.js
+++ src/components/MoviesCard.js
@@ -5,7 +5,7 @@ import { IMG_CDN_URL } from '../utility/constant'
 const MoviesCard = ({ poster_path }) => {
   if(!poster_path) return null ;
   return (
-    <div className='w-64 h-96 rounded-lg overflow-hidden px-4 border border-border/20 cursor-pointer
+    <div className='w-64 h-96 rounded-lg border-black overflow-hidden px-4 border border-border/20 cursor-pointer
     transition-transform duration-200 ease-[cubic-bezier(0.34,1.56,0.64,1)]
     hover:scale-[1.10] hover:shadow-lg hover:border-border/40 hover:z-10 hover:relative'>
         <img src={IMG_CDN_URL + poster_path} alt="movie-poster" />
